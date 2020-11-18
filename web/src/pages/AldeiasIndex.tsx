dad58a0d9efb12f22f0851bee3cfe5d6a6d8aa44
gustavo <gustavo-bergamo@live.com>
2020-11-18 20:18:39 -0300

    configurações gerais e gitignore unificado

--- web/src/pages/AldeiasIndex.tsx
+++ web/src/pages/AldeiasIndex.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FiArrowRight } from 'react-icons/fi'
 import api from '../services/api'
 
 import EnhancedTable from '../components/table'
