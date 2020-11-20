5b6be0aadc5defd1ebb8b181383cb403cd8f4149
gustavo <gustavo-bergamo@live.com>
2020-11-19 21:37:48 -0300

    conseguindo fazer o navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,57 @@
 import React from 'react';
 import Routes from './routes'
+import { Link } from 'react-router-dom'
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+
 import './styles/navbar.css'
+
+const history = createBrowserHistory();
 
 function App() {
 
   return (
-    <div> 
-      <div className="navbar">       
+    <Router history={history}>
+      <div>
+        <div className="navbar">
 
-        <div className="itensNavbar">
-          <a className="linkNavbar">
-            Teste1
+          <div className="itensNavbar">
+            <a className="linkNavbar" href="/users" >
+              DASHBOARD
           </a>
+          </div>
+
+          <div className="itensNavbar">
+            <Link to="/guerras" className="linkNavbar">
+              <a>GUERRAS</a>
+            </Link>
+          </div>
+
+          <div className="itensNavbar">
+            <a className="linkNavbar">
+              PERFIL
+          </a>
+          </div>
+
+          <div className="itensNavbar">
+            <a className="linkNavbar">
+              MUNDOS
+          </a>
+          </div>
+
+          <div className="itensNavbar">
+            <a className="linkNavbar">
+              CONFIGURAÇÕES
+          </a>
+          </div>
+
         </div>
 
-        <div className="itensNavbar">
-          <a className="linkNavbar">
-            Teste2
-          </a>
-        </div>
+        <Routes />
 
       </div>
 
-          <Routes/>
-    </div>
+    </Router>
   );
 }
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,20 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import AldeiasIndex from './pages/AldeiasIndex'
 import AldeiasUpdate from './pages/AldeiasUpdate'
 
-function Routes() {
+export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={AldeiasIndex} />
                 <Route path="/teste/:id" exact component={AldeiasUpdate} />
-                {/* <Route path="/app" component={} />
-                <Route path="/orphanages/create" component={} />
-                <Route path="/orphanages/:id" component={} />  */}
+                <Route path="/guerras" exact component={AldeiasIndex} />
             </Switch>
-
         </BrowserRouter>
     );
-
-
 }
-export default Routes
